1b1b2b73e683539ea6a196bfad0ab4170500c3e1
André Ricardo <offdedz@gmail.com>
2025-06-17 20:28:57 -0300

    Update prime_number_checker.py

--- prime_number_checker.py
+++ prime_number_checker.py
@@ -1,23 +1,23 @@
-# Verificador de Número Primo
+# Verificador de Número Primo - Projeto de Lógica de Programação
 
 # Entrada de dados
-n = int(input("Digite um número para verificar se é primo: "))
+n = int(input("Digite um número para verificar se ele é primo: "))
 
-# Variável de controle
+# Inicializa a variável de controle
 is_prime = True
 
-# Condição: números menores que 2 não são primos
+# Trata casos menores que 2
 if n < 2:
     is_prime = False
 else:
-    # Checando divisores de 2 até n-1
+    # Verifica possíveis divisores de 2 até n-1
     for i in range(2, n):
         if n % i == 0:
             is_prime = False
-            break
+            break  # Não precisa continuar verificando
 
 # Saída
 if is_prime:
-    print("Prime")
+    print(f"{n} é um número primo.")
 else:
-    print("Not Prime")
+    print(f"{n} não é um número primo.")
